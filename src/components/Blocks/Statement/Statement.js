a4cbe6c5c223c7ae59077e2478328c29e49e8d24
Chrish Dunne <hey@chrish.design>
2021-08-19 16:44:20 +0100

    feat: Added bones of statement block

---
+++ src/components/Blocks/Statement/Statement.js
@@ -0,0 +1,18 @@
+import React from "react";
+import PropTypes from "prop-types";
+import ReactMarkdown from "react-markdown";
+import * as S from "./Statement.styles";
+
+// ---
+
+const Statement = ({ copy }) => (
+  <S.Copy>
+    <ReactMarkdown>{copy}</ReactMarkdown>
+  </S.Copy>
+);
+
+Statement.propTypes = {
+  copy: PropTypes.string.isRequired,
+};
+
+export default Statement;
---
+++ src/components/Blocks/Statement/Statement.stories.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Statement from "./Statement";
+
+// ---
+
+export default {
+  title: "Blocks/Statement",
+};
+
+const Template = (args) => <Statement {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  copy: "We Believe _in_ **Great Ideas**. But We Also Believe That They Are **Just _the_ Beginnning**. You See, Once The Hard Work Of Coming _up_ With A Great Idea Is Complete... This Is **Where The _Real_ Work Begins.**",
+};
