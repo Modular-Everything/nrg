b12854948e4ed134b240c69046af38c0fd88bdc1
Chrish Dunne <hey@chrish.design>
2021-08-20 09:14:00 +0100

    feat: Added some good/bad examples

--- src/components/Blocks/Statement/Statement.stories.js
+++ src/components/Blocks/Statement/Statement.stories.js
@@ -13,3 +13,13 @@ export const Default = Template.bind({});
 Default.args = {
   copy: "We Believe _in_ **Great Ideas.** But We Also Believe That They Are **Just _the_ Beginnning.** You See, Once The Hard Work Of Coming _up_ With A Great Idea Is Complete... This Is **Where The _Real_ Work Begins.**",
 };
+
+export const GoodExample = Template.bind({});
+GoodExample.args = {
+  copy: "Ut enim ad **minim veniam,** quis nostrud *dolore* ullamco citus laboris nisi ut aliquip ex **earatur *sint* consequat.**",
+};
+
+export const BadExample = Template.bind({});
+BadExample.args = {
+  copy: "Duis aute irure dolor in **reprehenderit in voluptate *velit esse* cillum dolore eu fugiat** nulla pariatur. Excepteur sint **occaecat cupidatat** non proident, sunt in *culpa qui officia deserunt* mollit anim id est laborum.",
+};
