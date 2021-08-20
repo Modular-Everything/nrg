32a5de8c6e4c384b6d286a7cdec2f8efc2f2e8c4
Chrish Dunne <hey@chrish.design>
2021-08-20 09:02:12 +0100

    style: Added styling to statement

--- src/components/Blocks/Statement/Statement.stories.js
+++ src/components/Blocks/Statement/Statement.stories.js
@@ -11,5 +11,5 @@ const Template = (args) => <Statement {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
-  copy: "We Believe _in_ **Great Ideas**. But We Also Believe That They Are **Just _the_ Beginnning**. You See, Once The Hard Work Of Coming _up_ With A Great Idea Is Complete... This Is **Where The _Real_ Work Begins.**",
+  copy: "We Believe _in_ **Great Ideas.** But We Also Believe That They Are **Just _the_ Beginnning.** You See, Once The Hard Work Of Coming _up_ With A Great Idea Is Complete... This Is **Where The _Real_ Work Begins.**",
 };
--- src/components/Blocks/Statement/Statement.styles.js
+++ src/components/Blocks/Statement/Statement.styles.js
@@ -2,4 +2,40 @@ import styled from "styled-components";
 
 // ---
 
-export const Copy = styled.div``;
+export const Copy = styled.div`
+  p {
+    max-width: 96rem;
+    margin: 0 auto;
+    color: var(--black);
+    font-family: var(--avenue);
+    font-size: clamp(2.4rem, 8vw, 6.4rem);
+    letter-spacing: 0.05ch;
+    line-height: 1.1;
+    text-align: center;
+    text-transform: uppercase;
+  }
+
+  strong {
+    --padding: calc(var(--gutter) / 2);
+
+    display: inline;
+    align-items: center;
+    justify-content: center;
+    padding: 0 var(--padding);
+    background-color: var(--black);
+    box-decoration-break: clone;
+    box-shadow: var(--padding) 0 0 var(--black),
+      calc(var(--padding) * -1) 0 0 var(--black);
+    color: var(--white);
+    font-weight: normal;
+  }
+
+  em {
+    display: inline-block;
+    height: 100%;
+    transform: translateY(-50%);
+    font-family: var(--grotesque);
+    font-size: clamp(1.2rem, 3vw, 2.4rem);
+    font-style: normal;
+  }
+`;
