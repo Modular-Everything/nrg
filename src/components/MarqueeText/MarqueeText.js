2fc06d9ab1fd655229d91a69e064d75ad71f3008
Chrish Dunne <hey@chrish.design>
2021-09-02 16:45:17 +0100

    feat: Added placeholder MarqueeText

---
+++ src/components/MarqueeText/MarqueeText.js
@@ -0,0 +1,20 @@
+import React from "react";
+// import PropTypes from "prop-types";
+
+// ---
+
+const MarqueeText = () => {
+  console.log("MarqueeText");
+
+  return (
+    <div>
+      MarqueeText
+      <br />
+      https://css-tricks.com/how-to-do-knockout-text/
+    </div>
+  );
+};
+
+MarqueeText.propTypes = {};
+
+export default MarqueeText;
