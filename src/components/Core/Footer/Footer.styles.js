6d7faa99878f0f70a153ce1e15108761feb0e65d
Chrish Dunne <hey@chrish.design>
2021-09-24 11:20:56 +0100

    style: Removed margin-top

--- src/components/Core/Footer/Footer.styles.js
+++ src/components/Core/Footer/Footer.styles.js
@@ -4,7 +4,6 @@ import styled from "styled-components";
 
 export const Footer = styled.footer`
   min-height: 20rem;
-  margin-top: calc(var(--gutter) * 4);
   padding: var(--gutter);
   background-color: var(--black);
   color: var(--white);
