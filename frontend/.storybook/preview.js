75e5762b3cc2df4db607338f467da0ccefeae464
Chrish Dunne <hey@chrish.design>
2021-10-21 16:25:46 +0100

    fix: next/image not working

--- frontend/.storybook/preview.js
+++ frontend/.storybook/preview.js
@@ -1,8 +1,14 @@
+import * as nextImage from "next/image";
 import { addDecorator } from "@storybook/react";
 import { INITIAL_VIEWPORTS } from "@storybook/addon-viewport";
 
 import GlobalStyles from "../styles/GlobalStyles";
 import Typography from "../styles/Typography";
+
+Object.defineProperty(nextImage, "default", {
+  configurable: true,
+  value: (props) => <img {...props} />,
+});
 
 addDecorator((story) => (
   <>
