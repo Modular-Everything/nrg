347fae19b9c2cb5905ce1570f6fe13ad9ea5b3d8
Chrish Dunne <hey@chrish.design>
2021-10-22 12:18:09 +0100

    feat: Added another example

--- frontend/components/Blocks/ScrollingGallery/ScrollingGallery.stories.js
+++ frontend/components/Blocks/ScrollingGallery/ScrollingGallery.stories.js
@@ -15,7 +15,7 @@ const Template = (args) => <ScrollingGallery {...args} />;
 export const StandardExample = Template.bind({});
 StandardExample.args = {
   block: {
-    layout_type: "tall",
+    layout_type: "standard",
     items: [
       {
         id: 1,
@@ -23,6 +23,53 @@ StandardExample.args = {
           url: "https://res.cloudinary.com/modular-everything/image/upload/v1634233320/nrg/samantha_gades_f_I_Hoz_N_Wfcvs_unsplash_ea58004c52.jpg",
         },
         layout_type: "portrait",
+        link: null,
+        title: "",
+      },
+      {
+        id: 2,
+        image: {
+          url: "https://res.cloudinary.com/modular-everything/image/upload/v1634233320/nrg/samantha_gades_f_I_Hoz_N_Wfcvs_unsplash_ea58004c52.jpg",
+        },
+        layout_type: "portrait",
+        link: null,
+        title: "",
+      },
+      {
+        id: 3,
+        image: {
+          url: "https://res.cloudinary.com/modular-everything/image/upload/v1634233320/nrg/samantha_gades_f_I_Hoz_N_Wfcvs_unsplash_ea58004c52.jpg",
+        },
+        layout_type: "landscape",
+        link: {
+          slug: "#",
+        },
+        title: "We do more than tell stories",
+      },
+      {
+        id: 4,
+        image: {
+          url: "https://res.cloudinary.com/modular-everything/image/upload/v1634233320/nrg/samantha_gades_f_I_Hoz_N_Wfcvs_unsplash_ea58004c52.jpg",
+        },
+        layout_type: "portrait",
+        link: null,
+        title: "",
+      },
+    ],
+  },
+};
+
+export const TallExample = Template.bind({});
+TallExample.args = {
+  block: {
+    layout_type: "tall",
+    items: [
+      {
+        id: 1,
+        image: {
+          url: "https://res.cloudinary.com/modular-everything/image/upload/v1634233320/nrg/samantha_gades_f_I_Hoz_N_Wfcvs_unsplash_ea58004c52.jpg",
+        },
+        layout_type: "landscape",
         link: null,
         title: "",
       },
