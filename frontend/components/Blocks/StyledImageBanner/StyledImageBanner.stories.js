2ba95031954ef87a17acec975efa5cb6a72c9571
Chrish Dunne <hey@chrish.design>
2021-10-22 12:34:43 +0100

    feat: Updated marquee on demo

--- frontend/components/Blocks/StyledImageBanner/StyledImageBanner.stories.js
+++ frontend/components/Blocks/StyledImageBanner/StyledImageBanner.stories.js
@@ -19,7 +19,7 @@ SimpleBanner.args = {
       url: "https://res.cloudinary.com/modular-everything/image/upload/v1634233320/nrg/samantha_gades_f_I_Hoz_N_Wfcvs_unsplash_ea58004c52.jpg",
     },
     styled_copy:
-      "**We do more** than tell stories. We do more than tell **stories.**",
+      "**We do more** than tell stories\n\nWe do more than tell **stories**.\n\nWe do more **than tell** stories",
   },
 };
 
@@ -31,7 +31,7 @@ BannerWithDescription.args = {
     },
     descriptive_copy: `We do more than tell stories.\n\nWe create experiences that people tell stories about.`,
     styled_copy:
-      "**We do more** than tell stories. We do more than tell **stories.**",
+      "**We do more** than tell stories\n\nWe do more than tell **stories**.\n\nWe do more **than tell** stories",
   },
 };
 
@@ -43,7 +43,7 @@ BannerUsedAsHeader.args = {
     },
     descriptive_copy: `We do more than tell stories.\n\nWe create experiences that people tell stories about.`,
     styled_copy:
-      "**We do more** than tell stories. We do more than tell **stories.**",
+      "**We do more** than tell stories\n\nWe do more than tell **stories**.\n\nWe do more **than tell** stories",
     sits_below_menu: true,
     scroll_copy: "Find out more",
   },
