28983efc56dc12d2ac33b912ee4c5f3311ba58bc
chrish-d <hey@chrish.design>
2021-11-09 18:11:35 +0000

    feat: Added blur up

--- web/components/Elements/Image/Image.js
+++ web/components/Elements/Image/Image.js
@@ -1,7 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { useNextSanityImage } from "next-sanity-image";
 
-import { imageBuilder } from "../../../lib/sanity";
+import { config } from "../../../lib/config";
 import * as S from "./Image.styles";
 
 // ---
@@ -9,16 +10,16 @@ import * as S from "./Image.styles";
 const Image = ({ image, layout, ...rest }) => {
   if (!image?.asset) return null;
 
-  // eslint-disable-next-line no-unused-vars
-  const [assetType, _id, dimensions, filetype] = image?.asset?._ref.split("-");
-  const [width, height] = dimensions.split("x");
+  const imageProps = useNextSanityImage(config, image);
 
   return (
     <S.Image
-      src={imageBuilder(image).url()}
+      src={imageProps.src}
+      blurDataURL={imageProps.blurDataURL}
       layout={layout ?? "responsive"}
-      width={layout !== "fill" && width}
-      height={layout !== "fill" && height}
+      width={layout !== "fill" && imageProps.width}
+      height={layout !== "fill" && imageProps.height}
+      // placeholder="blur"
       alt={image?.alt}
       {...rest}
     />
