8b014e27a61f8516862f7ed8c32e8c9e8cfabfff
Chrish Dunne <hey@chrish.design>
2021-11-25 06:57:59 +0000

    chore: Removed unused prop types

--- web/components/Core/PreviewBanner/PreviewBanner.js
+++ web/components/Core/PreviewBanner/PreviewBanner.js
@@ -1,25 +1,16 @@
 import React from "react";
-import PropTypes from "prop-types";
 import Link from "next/link";
 
 import * as S from "./PreviewBanner.styles";
 
 // ---
 
-const PreviewBanner = ({ slug }) => (
+const PreviewBanner = () => (
   <S.PreviewBanner>
     <Link href="/api/exit-preview">
       <a>Preview Mode Activated!</a>
     </Link>
   </S.PreviewBanner>
 );
-
-PreviewBanner.propTypes = {
-  slug: PropTypes.string,
-};
-
-PreviewBanner.defaultProps = {
-  slug: null,
-};
 
 export default PreviewBanner;
