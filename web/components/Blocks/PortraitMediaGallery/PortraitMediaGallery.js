7eed7c4f0c75d0c477ccc53a056c1fb83c593612
Chrish Dunne <hey@chrish.design>
2022-01-20 17:43:12 +0000

    feat: Added some quick animations

--- web/components/Blocks/PortraitMediaGallery/PortraitMediaGallery.js
+++ web/components/Blocks/PortraitMediaGallery/PortraitMediaGallery.js
@@ -4,35 +4,38 @@ import PropTypes from "prop-types";
 import * as S from "./PortraitMediaGallery.styles";
 import Container from "../../Core/Container";
 import Image from "../../Elements/Image";
+import FadeIn from "../../Animations/FadeIn";
 
 export function PortraitMediaGallery({ block }) {
   return (
-    <Container>
-      <S.PortraitMediaGallery>
-        {block?.media?.map((item) => (
-          <article key={item._key}>
-            {item.mediaType === "image" ? (
-              <Image
-                className="content"
-                image={item.mediaImage}
-                layout="fill"
-                quality={90}
-              />
-            ) : (
-              <div className="content">
-                {/* eslint-disable-next-line jsx-a11y/media-has-caption */}
-                <video autoPlay muted loop>
-                  <source
-                    src={item.mediaVideo?.data?.url}
-                    type={item.mediaVideo?.data?.mimeType}
-                  />
-                </video>
-              </div>
-            )}
-          </article>
-        ))}
-      </S.PortraitMediaGallery>
-    </Container>
+    <FadeIn>
+      <Container>
+        <S.PortraitMediaGallery>
+          {block?.media?.map((item) => (
+            <article key={item._key}>
+              {item.mediaType === "image" ? (
+                <Image
+                  className="content"
+                  image={item.mediaImage}
+                  layout="fill"
+                  quality={90}
+                />
+              ) : (
+                <div className="content">
+                  {/* eslint-disable-next-line jsx-a11y/media-has-caption */}
+                  <video autoPlay muted loop>
+                    <source
+                      src={item.mediaVideo?.data?.url}
+                      type={item.mediaVideo?.data?.mimeType}
+                    />
+                  </video>
+                </div>
+              )}
+            </article>
+          ))}
+        </S.PortraitMediaGallery>
+      </Container>
+    </FadeIn>
   );
 }
 
--- web/components/Blocks/Tags/Tags.js
+++ web/components/Blocks/Tags/Tags.js
@@ -1,16 +1,19 @@
 import React from "react";
 import PropTypes from "prop-types";
 import * as S from "./Tags.styles";
+import FadeIn from "../../Animations/FadeIn";
 
 export function Tags({ block }) {
   return (
-    <S.Container>
-      <S.Tags tagColor={block?.tagColor}>
-        {block?.tags?.map((tag) => (
-          <li key={tag._key}>{tag}</li>
-        ))}
-      </S.Tags>
-    </S.Container>
+    <FadeIn>
+      <S.Container>
+        <S.Tags tagColor={block?.tagColor}>
+          {block?.tags?.map((tag) => (
+            <li key={tag._key}>{tag}</li>
+          ))}
+        </S.Tags>
+      </S.Container>
+    </FadeIn>
   );
 }
 
