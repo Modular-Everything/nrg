1a13e8caefee55c8a9e7c204d3db022d224908cc
Chrish Dunne <hey@chrish.design>
2022-02-18 20:43:19 +0000

    feat: Added Header

--- packages/web/components/core/Header/Header.js
+++ packages/web/components/core/Header/Header.js
@@ -1,5 +1,84 @@
+import Link from "next/link";
+
+import { Logo } from "../../icons/Logo";
+import { Container } from "../Container";
 import * as S from "./Header.styles";
 
 export function Header() {
-  return <S.Header>Header</S.Header>;
+  return (
+    <S.Header>
+      <Container>
+        <Link href="/">
+          <a>
+            <Logo />
+          </a>
+        </Link>
+
+        <S.Nav role="navigation">
+          <ul className="nav__wrap--outer">
+            <li className="nav__item--outer">
+              <label>
+                <span className="nav__item--title">We Are</span>
+                <input type="radio" name="test" aria-label="Open Menu" />
+                <ul className="nav__wrap--inner">
+                  <li className="nav__item--inner">
+                    <Link href="/">
+                      <a>Values &amp; Beliefs</a>
+                    </Link>
+                  </li>
+                  <li className="nav__item--inner">
+                    <Link href="/">
+                      <a>Team &amp; Culture</a>
+                    </Link>
+                  </li>
+                </ul>
+              </label>
+            </li>
+
+            <li className="divider" />
+
+            <li className="nav__item--outer">
+              <label>
+                <span className="nav__item--title">What</span>
+                <input type="radio" name="test" aria-label="Open Menu" />
+                <ul className="nav__wrap--inner">
+                  <li className="nav__item--inner">
+                    <Link href="/">
+                      <a>Values &amp; Beliefs</a>
+                    </Link>
+                  </li>
+                  <li className="nav__item--inner">
+                    <Link href="/">
+                      <a>Team &amp; Culture</a>
+                    </Link>
+                  </li>
+                </ul>
+              </label>
+            </li>
+
+            <li className="divider" />
+
+            <li className="nav__item--outer">
+              <label>
+                <span className="nav__item--title">We Do</span>
+                <input type="radio" name="test" aria-label="Open Menu" />
+                <ul className="nav__wrap--inner">
+                  <li className="nav__item--inner">
+                    <Link href="/">
+                      <a>Values &amp; Beliefs</a>
+                    </Link>
+                  </li>
+                  <li className="nav__item--inner">
+                    <Link href="/">
+                      <a>Team &amp; Culture</a>
+                    </Link>
+                  </li>
+                </ul>
+              </label>
+            </li>
+          </ul>
+        </S.Nav>
+      </Container>
+    </S.Header>
+  );
 }
--- packages/web/components/core/Header/Header.styles.js
+++ packages/web/components/core/Header/Header.styles.js
@@ -1,3 +1,64 @@
 import styled from "styled-components";
 
-export const Header = styled.header``;
+export const Header = styled.header`
+  background-color: var(--nrg-black);
+  color: var(--white);
+  padding: clamp(1.6rem, 6vw, 4rem) 0;
+  position: relative;
+
+  .container {
+    display: flex;
+    justify-content: space-between;
+  }
+`;
+
+export const Nav = styled.nav`
+  .nav__wrap--outer {
+    display: flex;
+    justify-content: flex-end;
+    gap: 1.6rem;
+    position: relative;
+  }
+
+  .nav__item--outer:last-of-type .nav__wrap--inner {
+    right: 0;
+  }
+
+  .divider {
+    width: 0.1rem;
+    background-color: #3b3b3b;
+  }
+
+  .nav__wrap--inner {
+    display: none;
+  }
+
+  input[type="radio"] {
+    display: none;
+
+    &:checked + .nav__wrap--inner {
+      display: flex;
+    }
+  }
+
+  .nav__item--title {
+    cursor: pointer;
+    text-transform: uppercase;
+    font-size: 2.4rem;
+    line-height: 3.2rem;
+
+    &:hover {
+      @supports (-webkit-text-stroke: 1px black) {
+        -webkit-text-stroke: 1px var(--nrg-red);
+        -webkit-text-fill-color: transparent;
+      }
+    }
+  }
+
+  .nav__wrap--inner {
+    position: absolute;
+    background-color: var(--black);
+    top: 100%;
+    flex-direction: column;
+  }
+`;
