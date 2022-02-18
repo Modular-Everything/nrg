3fc09eb0396e4e49a6e1d6890869b93ea86ef3bb
Chrish Dunne <hey@chrish.design>
2022-02-18 15:28:48 +0000

    fix: Further linting

--- packages/web/pages/_app.js
+++ packages/web/pages/_app.js
@@ -1,7 +1,8 @@
-import '../styles/globals.css'
+/* eslint-disable import/no-default-export */
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
