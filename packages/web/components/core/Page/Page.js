7180672a2f686c65595af22a2d07e7b6f7699935
Chrish Dunne <hey@chrish.design>
2022-02-19 09:53:29 +0000

    feat: Move Header and Footer to [[...slug]].js

--- packages/web/components/core/Page/Page.js
+++ packages/web/components/core/Page/Page.js
@@ -1,26 +1,11 @@
-import Head from "next/head";
 import PropTypes from "prop-types";
-
-import { Footer } from "../Footer";
-import { Header } from "../Header";
 
 export function Page({ data }) {
   return (
-    <>
-      <Head>
-        <title>{data?.title}</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <Header />
-
-      <main>
-        <h1>{data?.title}</h1>
-        Loaded in <pre style={{ display: "inline" }}>Page.js</pre>
-      </main>
-
-      <Footer />
-    </>
+    <main>
+      <h1>{data?.title}</h1>
+      Loaded in <pre style={{ display: "inline" }}>Page.js</pre>
+    </main>
   );
 }
 
