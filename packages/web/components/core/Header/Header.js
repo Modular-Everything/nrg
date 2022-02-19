57e35f3b8ecb47766e32e45371b9dcf812bbd529
Chrish Dunne <hey@chrish.design>
2022-02-19 09:53:47 +0000

    feat: Use data

--- packages/web/components/core/Header/Header.js
+++ packages/web/components/core/Header/Header.js
@@ -1,10 +1,11 @@
 import Link from "next/link";
+import PropTypes from "prop-types";
 
 import { Logo } from "../../icons/Logo";
 import { Container } from "../Container";
 import * as S from "./Header.styles";
 
-export function Header() {
+export function Header({ data }) {
   return (
     <S.Header>
       <Container>
@@ -16,69 +17,30 @@ export function Header() {
 
         <S.Nav role="navigation">
           <ul className="nav__wrap--outer">
-            <li className="nav__item--outer">
-              <label>
-                <span className="nav__item--title">We Are</span>
-                <input type="radio" name="test" aria-label="Open Menu" />
-                <ul className="nav__wrap--inner">
-                  <li className="nav__item--inner">
-                    <Link href="/">
-                      <a>Values &amp; Beliefs</a>
-                    </Link>
-                  </li>
-                  <li className="nav__item--inner">
-                    <Link href="/">
-                      <a>Team &amp; Culture</a>
-                    </Link>
-                  </li>
-                </ul>
-              </label>
-            </li>
-
-            <li className="divider" />
-
-            <li className="nav__item--outer">
-              <label>
-                <span className="nav__item--title">What</span>
-                <input type="radio" name="test" aria-label="Open Menu" />
-                <ul className="nav__wrap--inner">
-                  <li className="nav__item--inner">
-                    <Link href="/">
-                      <a>Values &amp; Beliefs</a>
-                    </Link>
-                  </li>
-                  <li className="nav__item--inner">
-                    <Link href="/">
-                      <a>Team &amp; Culture</a>
-                    </Link>
-                  </li>
-                </ul>
-              </label>
-            </li>
-
-            <li className="divider" />
-
-            <li className="nav__item--outer">
-              <label>
-                <span className="nav__item--title">We Do</span>
-                <input type="radio" name="test" aria-label="Open Menu" />
-                <ul className="nav__wrap--inner">
-                  <li className="nav__item--inner">
-                    <Link href="/">
-                      <a>Values &amp; Beliefs</a>
-                    </Link>
-                  </li>
-                  <li className="nav__item--inner">
-                    <Link href="/">
-                      <a>Team &amp; Culture</a>
-                    </Link>
-                  </li>
-                </ul>
-              </label>
-            </li>
+            {data?.navigation.map((topLevel) => (
+              <li key={topLevel._key} className="nav__item--outer">
+                <label>
+                  <span className="nav__item--title">{topLevel.label}</span>
+                  <input type="radio" name="nav" aria-label="Open Menu" />
+                  <ul className="nav__wrap--inner">
+                    {topLevel.children.map((children) => (
+                      <li key={children._key} className="nav__item--inner">
+                        <Link href={children.target.slug.current}>
+                          <a>{children.label}</a>
+                        </Link>
+                      </li>
+                    ))}
+                  </ul>
+                </label>
+              </li>
+            ))}
           </ul>
         </S.Nav>
       </Container>
     </S.Header>
   );
 }
+
+Header.propTypes = {
+  data: PropTypes.object.isRequired,
+};
