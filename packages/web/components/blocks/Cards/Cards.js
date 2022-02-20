e16c088ac5e6a3636d9684aebef9a1e42aab2b02
Chrish Dunne <hey@chrish.design>
2022-02-20 18:22:22 +0000

    feat: Added 'cards' component

---
+++ packages/web/components/blocks/Cards/Cards.js
@@ -0,0 +1,5 @@
+import * as S from "./Cards.styles";
+
+export function Cards() {
+  return <S.Cards>Cards</S.Cards>;
+}
--- packages/web/components/blocks/LandscapeCards/LandscapeCards.js
+++
@@ -1,5 +0,0 @@
-import * as S from "./LandscapeCards.styles";
-
-export function LandscapeCards() {
-  return <S.LandscapeCards>LandscapeCards</S.LandscapeCards>;
-}
