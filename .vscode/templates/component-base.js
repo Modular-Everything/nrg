93a18ac081807d28c64643d662d871ffdca799d5
Chrish Dunne <hey@chrish.design>
2022-02-20 19:18:12 +0000

    feat: Add base template

--- .vscode/templates/component-base.js
+++ .vscode/templates/component-base.js
@@ -1,1 +1,18 @@
 ##@@## ${relativeFileDirnameSplit[-1]}
+import PropTypes from 'prop-types';
+
+import * as S from './${relativeFileDirnameSplit[-1]}.styles';
+
+export function ${relativeFileDirnameSplit[-1]}({ data }) {
+  console.log("${relativeFileDirnameSplit[-1]}", data);
+
+  return(
+    <S.${relativeFileDirnameSplit[-1]}>
+      ${relativeFileDirnameSplit[-1]}
+    </S.${relativeFileDirnameSplit[-1]}>
+  )
+}
+
+${relativeFileDirnameSplit[-1]}.propTypes = {
+  data: PropTypes.object.isRequired,
+}
