c5891ae24c20ac1487658e6c11b75cb273327def
Chrish Dunne <hey@chrish.design>
2022-02-20 19:18:29 +0000

    feat: Added Horizontal Card

---
+++ packages/web/components/elements/Card/Card.js
@@ -0,0 +1,77 @@
+/* eslint-disable no-unused-vars */
+import Link from "next/link";
+import PropTypes from "prop-types";
+
+import { Image } from "../Image/Image";
+import * as S from "./Card.styles";
+
+export function Card({ data }) {
+  if (!data?.image || !data?.linkToRef?.link) {
+    return <pre>Error: Missing required image</pre>;
+  }
+
+  const [assetType, _id, dimensions, filetype] =
+    data.image.asset._ref.split("-");
+  const [width, height] = dimensions.split("x");
+  const isLandscape = Number(width) > Number(height);
+
+  function Content() {
+    return (
+      <>
+        <div className="card__image">
+          <Image
+            src={data?.image}
+            alt={data?.title || ""}
+            layout="fill"
+            objectFit="cover"
+          />
+        </div>
+
+        <div className="card__meta">
+          {data?.subtitle && <h5>{data.subtitle}</h5>}
+          {data?.title && <h4>{data.title}</h4>}
+          <span>{data?.linkToRef?.label || "View Project"} &rarr;</span>
+        </div>
+      </>
+    );
+  }
+
+  return (
+    <S.Card className={isLandscape ? "landscape" : "portrait"}>
+      {data?.linkToRef?.link?.slug ? (
+        <Link href={data?.linkToRef?.link?.slug?.current}>
+          <a>
+            <Content />
+          </a>
+        </Link>
+      ) : (
+        <Content />
+      )}
+    </S.Card>
+  );
+}
+
+Card.propTypes = {
+  data: PropTypes.shape({
+    copy: PropTypes.string,
+    image: PropTypes.object.isRequired,
+    linkToRef: PropTypes.shape({
+      label: PropTypes.string,
+      link: PropTypes.object.isRequired,
+    }),
+    subtitle: PropTypes.string,
+    title: PropTypes.string,
+  }).isRequired,
+};
+
+Card.defaultProps = {
+  data: {
+    copy: null,
+    linkToRef: {
+      label: null,
+      link: null,
+    },
+    subtitle: null,
+    title: null,
+  },
+};
---
+++ packages/web/components/elements/Card/Card.styles.js
@@ -0,0 +1,51 @@
+import styled from "styled-components";
+
+export const Card = styled.article`
+  &.landscape {
+    .card__image {
+      aspect-ratio: 512/315;
+    }
+  }
+
+  .card__image {
+    position: relative;
+    overflow: hidden;
+    border-radius: 0.8rem;
+  }
+
+  .card__meta {
+    margin-top: 1.2rem;
+
+    h4,
+    h5 {
+      margin: 0;
+      padding: 0;
+    }
+
+    h5 {
+      font-family: var(--grotesque);
+      font-size: 1.6rem;
+      line-height: 2.4rem;
+      color: var(--nrg-lt-grey);
+      text-transform: uppercase;
+    }
+
+    h4 {
+      font-family: var(--simplon);
+      font-style: normal;
+      font-weight: normal;
+      font-size: clamp(1.8rem, 3vw, 2.8rem);
+      line-height: clamp(2rem, 3vw, 3rem);
+      margin-top: 0.4rem;
+    }
+
+    span {
+      font-family: var(--simplon);
+      font-size: 1.6rem;
+      line-height: 2.4rem;
+      color: var(--nrg-red);
+      margin-top: 1.2rem;
+      display: block;
+    }
+  }
+`;
