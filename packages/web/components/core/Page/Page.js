4043d367a1dd267513989ce65af27b5079a128b8
Chrish Dunne <hey@chrish.design>
2022-02-20 12:45:19 +0000

    feat: Added styled component

--- packages/web/components/core/Page/Page.js
+++ packages/web/components/core/Page/Page.js
@@ -1,11 +1,13 @@
 import PropTypes from "prop-types";
 
+import * as S from "./Page.styles";
+
 export function Page({ data }) {
   return (
-    <main>
+    <S.Page>
       <h1>{data?.title}</h1>
       Loaded in <pre style={{ display: "inline" }}>Page.js</pre>
-    </main>
+    </S.Page>
   );
 }
 
