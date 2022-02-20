57c0e6a5818e7a12a0380b8a6565c30cbdcf3a73
Chrish Dunne <hey@chrish.design>
2022-02-20 19:18:35 +0000

    feat: Accept cards

--- packages/web/components/blocks/Cards/Cards.js
+++ packages/web/components/blocks/Cards/Cards.js
@@ -1,5 +1,18 @@
+import { Container } from "../../core/Container";
+import { Card } from "../../elements/Card/Card";
 import * as S from "./Cards.styles";
 
-export function Cards() {
-  return <S.Cards>Cards</S.Cards>;
+export function Cards({ data }) {
+  return (
+    <S.Cards>
+      <Container>
+        {data?.allCards?.map((card) => {
+          const { _key, image, linkToRef, subtitle, title } = card;
+          return (
+            <Card key={_key} data={{ image, linkToRef, subtitle, title }} />
+          );
+        })}
+      </Container>
+    </S.Cards>
+  );
 }
--- packages/web/components/blocks/Cards/Cards.styles.js
+++ packages/web/components/blocks/Cards/Cards.styles.js
@@ -1,3 +1,14 @@
 import styled from "styled-components";
 
-export const Cards = styled.section``;
+export const Cards = styled.section`
+  padding: 1.6rem 0;
+
+  .container {
+    --min: 32rem;
+    --gap: 1rem;
+
+    display: grid;
+    grid-gap: var(--gap);
+    grid-template-columns: repeat(auto-fit, minmax(min(100%, var(--min)), 1fr));
+  }
+`;
