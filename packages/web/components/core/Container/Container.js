eb91ee6b280016eee8994ac92d29755d3c4f1648
Chrish Dunne <hey@chrish.design>
2022-02-21 22:00:14 +0000

    feat: Accept ...rest props

--- packages/web/components/core/Container/Container.js
+++ packages/web/components/core/Container/Container.js
@@ -2,8 +2,12 @@ import PropTypes from "prop-types";
 
 import * as S from "./Container.styles";
 
-export function Container({ children }) {
-  return <S.Container className="container">{children}</S.Container>;
+export function Container({ children, ...rest }) {
+  return (
+    <S.Container className="container" {...rest}>
+      {children}
+    </S.Container>
+  );
 }
 
 Container.propTypes = {
