5aea34bb7cb9dc4c2551db3fb8a1618a9b870131
Chrish Dunne <hey@chrish.design>
2022-02-21 11:01:11 +0000

    feat: Added schema

--- packages/studio/schemas/blocks/ProjectNavigation.js
+++ packages/studio/schemas/blocks/ProjectNavigation.js
@@ -1,12 +1,21 @@
+import { title, subtitle, image, linkToRef } from "../fields";
+
 export default {
   name: "projectNavigation",
   title: "Project Navigation",
   type: "object",
   fields: [
     {
-      name: "placeholder",
-      title: "Work in progress",
-      type: "string",
+      name: "items",
+      type: "array",
+      validation: (Rule) => Rule.length(2),
+      of: [
+        {
+          name: "item",
+          type: "object",
+          fields: [title, subtitle, image, linkToRef],
+        },
+      ],
     },
   ],
 };
