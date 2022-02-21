248ac233ba18365f54a8bec527df9e130567c4a6
Chrish Dunne <hey@chrish.design>
2022-02-21 11:42:43 +0000

    feat: Added all the things

--- packages/web/components/blocks/ProjectNavigation/ProjectNavigation.js
+++ packages/web/components/blocks/ProjectNavigation/ProjectNavigation.js
@@ -1,5 +1,56 @@
+import Link from "next/link";
+import PropTypes from "prop-types";
+
+import { Image } from "../../elements/Image";
 import * as S from "./ProjectNavigation.styles";
 
-export function ProjectNavigation() {
-  return <S.ProjectNavigation>ProjectNavigation</S.ProjectNavigation>;
+export function ProjectNavigation({ data }) {
+  return (
+    <S.ProjectNavigation>
+      {data?.items?.map((item) => (
+        <Link key={item._key} href={item?.linkToRef?.link?.slug?.current}>
+          <a>
+            <div className="projectNav__image">
+              <S.Skrim />
+              <Image
+                src={item?.image}
+                alt={item?.title || ""}
+                layout="fill"
+                objectFit="cover"
+              />
+            </div>
+
+            <div className="projectNav__meta">
+              {item?.subtitle && <h5>{item.subtitle}</h5>}
+              {item?.title && <h4>{item.title}</h4>}
+              <span>{item?.linkToRef?.label || "View Project"} &rarr;</span>
+            </div>
+          </a>
+        </Link>
+      ))}
+    </S.ProjectNavigation>
+  );
 }
+
+ProjectNavigation.propTypes = {
+  data: PropTypes.shape({
+    image: PropTypes.object.isRequired,
+    linkToRef: PropTypes.shape({
+      label: PropTypes.string,
+      link: PropTypes.object.isRequired,
+    }),
+    subtitle: PropTypes.string,
+    title: PropTypes.string,
+  }).isRequired,
+};
+
+ProjectNavigation.defaultProps = {
+  data: {
+    linkToRef: {
+      label: null,
+      link: null,
+    },
+    subtitle: null,
+    title: null,
+  },
+};
--- packages/web/components/blocks/ProjectNavigation/ProjectNavigation.styles.js
+++ packages/web/components/blocks/ProjectNavigation/ProjectNavigation.styles.js
@@ -1,3 +1,83 @@
 import styled from "styled-components";
 
-export const ProjectNavigation = styled.section``;
+export const ProjectNavigation = styled.section`
+  --min: 32rem;
+
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(min(100%, var(--min)), 1fr));
+  position: relative;
+  height: 37.2rem;
+
+  a {
+    position: relative;
+  }
+
+  .projectNav__image {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 0;
+  }
+
+  .projectNav__meta {
+    position: relative;
+    z-index: 10;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    gap: 2.4rem;
+    color: var(--white);
+    text-align: center;
+    padding: 1.6rem;
+
+    @media (min-width: 655px) {
+      padding-bottom: 4.8rem;
+      justify-content: flex-end;
+    }
+
+    h4,
+    h5 {
+      margin: 0;
+      padding: 0;
+    }
+
+    h5 {
+      font-family: var(--grotesque);
+      font-size: 1.6rem;
+      line-height: 2rem;
+      text-transform: uppercase;
+    }
+
+    h4 {
+      font-family: var(--simplon);
+      font-style: normal;
+      font-weight: 600;
+      font-size: clamp(1.8rem, 3vw, 2.8rem);
+      line-height: clamp(2rem, 3vw, 3rem);
+    }
+
+    span {
+      font-family: var(--simplon);
+      font-size: 1.6rem;
+      font-weight: 600;
+      line-height: 2.4rem;
+      display: block;
+      color: var(--nrg-red);
+    }
+  }
+`;
+
+export const Skrim = styled.div`
+  position: absolute;
+  z-index: 5;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(0, rgba(0, 0, 0, 0.8) 0, rgba(0, 0, 0, 0) 100%);
+`;
