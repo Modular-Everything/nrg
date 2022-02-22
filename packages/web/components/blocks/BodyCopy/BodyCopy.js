6b76ded8f4fdfb292d13c233d9babd9085d818e3
Chrish Dunne <hey@chrish.design>
2022-02-22 16:48:10 +0000

    chore: Remove clg

--- packages/web/components/blocks/BodyCopy/BodyCopy.js
+++ packages/web/components/blocks/BodyCopy/BodyCopy.js
@@ -4,14 +4,10 @@ import { Container } from "../../core/Container";
 import * as S from "./BodyCopy.styles";
 
 export function BodyCopy({ data }) {
-  console.log(data);
-
-  const components = {};
-
   return (
     <S.BodyCopy columns={data?.columns}>
       <Container style={data?.columns === 1 ? { maxWidth: "68rem" } : {}}>
-        <PortableText value={data.bodyCopy.copy} components={components} />
+        <PortableText value={data.bodyCopy.copy} />
       </Container>
     </S.BodyCopy>
   );
