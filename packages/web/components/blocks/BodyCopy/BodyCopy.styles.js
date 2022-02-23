16f021eb2d6286d4029bdf252c902381ba521b0f
Chrish Dunne <hey@chrish.design>
2022-02-23 14:50:34 +0000

    feat: Changes to padding and font sizes

--- packages/web/components/blocks/BodyCopy/BodyCopy.styles.js
+++ packages/web/components/blocks/BodyCopy/BodyCopy.styles.js
@@ -1,7 +1,8 @@
 import styled from "styled-components";
 
 export const BodyCopy = styled.section`
-  padding: 8rem 0;
+  padding: 2.4rem 0;
+  background-color: ${({ theme }) => theme.background};
 
   .container {
     @media (min-width: 640px) {
@@ -34,7 +35,7 @@ export const BodyCopy = styled.section`
   ol {
     margin: 0 0 1.6rem 0;
     font-family: var(--simplonNorm);
-    font-size: clamp(1.4rem, 3vw, 1.8rem);
+    font-size: clamp(1.4rem, 3vw, 1.6rem);
     line-height: clamp(2rem, 4vw, 2.4rem);
 
     &:last-of-type {
@@ -45,5 +46,15 @@ export const BodyCopy = styled.section`
     & + h3 {
       margin-top: 4rem;
     }
+  }
+
+  .linkToRef {
+    margin-top: 1.6rem;
+    padding: 0.4rem 0.8rem 0.4rem 0;
+    color: ${({ theme }) => theme.cta};
+  }
+
+  a:hover .arrow {
+    transform: translateX(0.8rem);
   }
 `;
