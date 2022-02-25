f3a733588553b9a1d24dcca443208658a773e4a0
Chrish Dunne <hey@chrish.design>
2022-02-25 13:33:25 +0000

    feat: Added schema

--- packages/studio/schemas/blocks/LogoGrid.js
+++ packages/studio/schemas/blocks/LogoGrid.js
@@ -1,12 +1,29 @@
+import { bodyCopy } from "../fields";
+
 export default {
   name: "logoGrid",
   title: "Logo Grid",
   type: "object",
   fields: [
+    bodyCopy,
     {
-      name: "placeholder",
-      title: "Work in progress",
-      type: "string",
+      name: "logos",
+      title: "Logos",
+      type: "array",
+      of: [
+        {
+          name: "logo",
+          title: "Logo",
+          type: "image",
+          fields: [
+            {
+              name: "alt",
+              title: "Alt",
+              type: "string",
+            },
+          ],
+        },
+      ],
     },
   ],
 };
