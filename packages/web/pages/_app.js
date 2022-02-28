e47c2a1071611df9c8f31d653c688404d6eda527
Chrish Dunne <hey@chrish.design>
2022-02-28 21:31:36 +0000

    feat: Added LogRocket

--- packages/web/pages/_app.js
+++ packages/web/pages/_app.js
@@ -1,8 +1,15 @@
 import "../styles/sanitize.css";
 import "../styles/globals.css";
+import LogRocket from "logrocket";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script id="logrocket">{LogRocket.init("rpltij/nrg")}</Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
