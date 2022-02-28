08b0a314f54644351f5035ce3b2f6efc67072f1d
Chrish Dunne <hey@chrish.design>
2022-02-28 19:13:40 +0000

    fix: Build error?

--- packages/web/pages/[[...slug]].js
+++ packages/web/pages/[[...slug]].js
@@ -100,7 +100,13 @@ export async function getStaticProps({ params, preview = false }) {
   return {
     props: {
       preview,
-      data: { query, queryParams, docType, page, globalSettings },
+      data: {
+        query,
+        queryParams,
+        docType,
+        page: page ?? [],
+        globalSettings: globalSettings ?? [],
+      },
     },
   };
 }
