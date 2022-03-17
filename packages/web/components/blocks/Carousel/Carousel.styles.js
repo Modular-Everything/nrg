447f2bc201f729f7f7447c2ec45fc58ee06bcfd3
Chrish Dunne <hey@chrish.design>
2022-03-17 10:30:56 +0000

    fix: Responsive carousels cropping off

--- packages/web/components/blocks/Carousel/Carousel.styles.js
+++ packages/web/components/blocks/Carousel/Carousel.styles.js
@@ -8,19 +8,30 @@ export const Carousel = styled.section`
     overflow: visible;
   }
 
-  .landscape {
-    width: 51.2rem;
+  .landscape,
+  .portrait {
+    width: 100%;
 
     .imageElement {
-      aspect-ratio: 512 / 320;
+      aspect-ratio: 3/4;
     }
   }
 
-  .portrait {
-    width: 24.7rem;
+  @media (min-width: 550px) {
+    .landscape {
+      width: 51.2rem;
 
-    .imageElement {
-      aspect-ratio: 247 / 320;
+      .imageElement {
+        aspect-ratio: 512 / 320;
+      }
+    }
+
+    .portrait {
+      width: 24.7rem;
+
+      .imageElement {
+        aspect-ratio: 247 / 320;
+      }
     }
   }
 `;
