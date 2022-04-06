20939bfd59c34b1e3b94a7c2ff5fd68c9791f78b
Chrish Dunne <hey@chrish.design>
2022-04-06 09:20:56 +0100

    feat: Disable auto play on contained videos

--- packages/web/components/blocks/Video/Video.js
+++ packages/web/components/blocks/Video/Video.js
@@ -8,7 +8,7 @@ export function Video({ data }) {
   return (
     <Container>
       <S.Video>
-        <Vimeo url={data?.vimeo} contained />
+        <Vimeo url={data?.vimeo} contained paused />
       </S.Video>
     </Container>
   );
--- packages/web/components/elements/Vimeo/Vimeo.js
+++ packages/web/components/elements/Vimeo/Vimeo.js
@@ -3,14 +3,16 @@ import PropTypes from "prop-types";
 
 import * as S from "./Vimeo.styles";
 
-export function Vimeo({ url, contained }) {
+export function Vimeo({ url, contained, paused }) {
   return (
     <S.Vimeo className={`vimeo ${contained ? "contained" : "full"}`}>
       {url && (
         <iframe
           src={`https://player.vimeo.com/video/${
             getVideoId(url).id
-          }?h=0f405f633e&amp;muted=1&amp;autoplay=1&amp;loop=1&amp;background=1&amp;app_id=122963`}
+          }?h=0f405f633e${
+            !paused && "&amp;muted=1&amp;autoplay=1&amp;loop=1&amp;background=1"
+          }&amp;app_id=122963`}
           title="Vimeo"
           width="1920"
           height="1080"
@@ -26,8 +28,10 @@ export function Vimeo({ url, contained }) {
 Vimeo.propTypes = {
   url: PropTypes.string.isRequired,
   contained: PropTypes.bool,
+  paused: PropTypes.bool,
 };
 
 Vimeo.defaultProps = {
   contained: false,
+  paused: false,
 };
