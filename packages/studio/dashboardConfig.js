656bebb3c48de2b6fa63ae13dc97151cbf6513f4
Chrish Dunne <yo@modular-everything.com>
2022-11-01 19:15:25 +0000

    fix: Replace netlify deployment tool

--- packages/studio/dashboardConfig.js
+++ packages/studio/dashboardConfig.js
@@ -3,20 +3,14 @@ export default {
     {
       name: "netlify",
       options: {
-        title: "My Netlify deploys",
+        title: "Deploy site",
         sites: [
           {
-            title: "Sanity Studio",
-            apiId: "xxxxx-yyyy-zzzz-xxxx-yyyyyyyy",
-            buildHookId: "xxxyyyxxxyyyyxxxyyy",
-            name: "sanity-gatsby-blog-20-studio",
-          },
-          {
-            title: "Website",
-            apiId: "yyyyy-xxxxx-zzzz-xxxx-yyyyyyyy",
-            buildHookId: "yyyyxxxxxyyyxxdxxx",
-            name: "sanity-gatsby-blog-20-web",
-            url: "https://my-sanity-deployment.com",
+            title: "NRG V2",
+            apiId: "f06b5bae-3b08-4cb3-ab6c-3d2cca356d75",
+            buildHookId: "63616df0d3ae38373bdd92f4",
+            name: "nrg-v2",
+            url: "https://nrg-v2.netlify.app/",
           },
         ],
       },
