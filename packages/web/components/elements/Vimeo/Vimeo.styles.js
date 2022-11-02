c235d7de583ac9f1b4af6bded8b5d14decebf1bb
Chrish Dunne <yo@modular-everything.com>
2022-11-02 19:44:46 +0000

    feat: Better aspect ratio handling

--- packages/web/components/elements/Vimeo/Vimeo.styles.js
+++ packages/web/components/elements/Vimeo/Vimeo.styles.js
@@ -1,34 +1,42 @@
 import styled from "styled-components";
 
 export const Vimeo = styled.div`
-  --video-width: 1920;
-  --video-height: 1080;
-
-  iframe {
-    width: 100%;
-    height: 100%;
-    aspect-ratio: var(--video-width) / var(--video-height);
-  }
-
   &.contained {
     border-radius: 0.8rem;
     overflow: hidden;
     box-shadow: var(--drop);
+
+    iframe {
+      width: 100%;
+      height: 100%;
+      aspect-ratio: 16/9;
+    }
   }
 
   &.full {
+    position: relative;
     overflow: hidden;
-    max-width: 100%;
+    width: 100vw;
+    height: 100vh;
     background: var(--nrg-black);
 
     iframe {
-      background: var(--nrg-black);
       position: absolute;
-      top: 0;
-      pointer-events: none;
-      left: 0;
-      width: 100%;
-      height: 100%;
+      top: 50%;
+      left: 50%;
+      width: 100vw;
+      height: 100vh;
+      transform: translate(-50%, -50%);
+
+      @media (min-aspect-ratio: 16/9) {
+        /* height = 100 * (9 / 16) = 56.25 */
+        height: 56.25vw;
+      }
+
+      @media (max-aspect-ratio: 16/9) {
+        /* width = 100 / (9 / 16) = 177.777777 */
+        width: 177.78vh;
+      }
     }
   }
 `;
