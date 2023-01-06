be9601c78bc475df643fee6d9e0482a141592f04
Chrish Dunne <yo@modular-everything.com>
2023-01-06 11:04:44 +0000

    feat: Focus search when open

--- packages/web/components/core/Search/Search.js
+++ packages/web/components/core/Search/Search.js
@@ -2,18 +2,26 @@ import { FiSearch } from "react-icons/fi";
 
 import * as S from "./Search.styles.js";
 
-function toggleSearch() {
+function toggleSearch(focus) {
   const search = document.querySelector("#search");
+  const input = search.querySelector(".aa-Input");
   const skrim = document.querySelector("#search-skrim");
   const body = document.querySelector("body");
+
   search.classList.toggle("open");
   skrim.classList.toggle("open");
   body.classList.toggle("disable-scroll");
+
+  if (focus) {
+    input.focus();
+  } else {
+    input.blur();
+  }
 }
 
 export function SearchIcon() {
   return (
-    <S.SearchButton type="button" onClick={() => toggleSearch()}>
+    <S.SearchButton type="button" onClick={() => toggleSearch(true)}>
       <FiSearch />
     </S.SearchButton>
   );
@@ -25,6 +33,10 @@ export function Search() {
 
 export function SearchSkrim() {
   return (
-    <S.Skrim id="search-skrim" type="button" onClick={() => toggleSearch()} />
+    <S.Skrim
+      id="search-skrim"
+      type="button"
+      onClick={() => toggleSearch(false)}
+    />
   );
 }
