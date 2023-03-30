dc4a706e6ddcf4133fb0e99e0b159761a796ff78
Chrish Dunne <yo@modular-everything.com>
2023-03-30 15:26:15 +0100

    feat: Added scripts

--- packages/web/pages/_app.js
+++ packages/web/pages/_app.js
@@ -29,6 +29,7 @@ function MyApp({ Component, pageProps }) {
           });
         `}
       </Script>
+
       <Script id="google-analytics" strategy="afterInteractive">
         {`
           (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
@@ -40,6 +41,11 @@ function MyApp({ Component, pageProps }) {
           ga('send', 'pageview');
         `}
       </Script>
+
+      <Script id="google-tag-manager">
+        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','GTM-NB3FPC2');`}
+      </Script>
+
       <Component {...pageProps} />
     </>
   );
