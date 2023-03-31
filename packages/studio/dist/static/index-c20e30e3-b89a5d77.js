03666babd7d58ee40e80ccd103fa7d0a0cc9e8a4
Chrish Dunne <yo@modular-everything.com>
2023-03-31 10:31:31 +0100

    chore: Build/deploy sanity v3

---
+++ packages/studio/dist/static/index-c20e30e3-b89a5d77.js
@@ -0,0 +1,1 @@
+import{DocumentPaneProvider as t,DocumentPane as a}from"./index-63f13ccc-44616122.js";import"./sanity-d3cb86e3.js";export{t as DocumentPaneProvider,a as default};
---
+++ packages/studio/sanity.cli.js
@@ -0,0 +1,8 @@
+import { defineCliConfig } from "sanity/cli";
+
+export default defineCliConfig({
+  api: {
+    projectId: "vg8y9z13",
+    dataset: "production-new",
+  },
+});
